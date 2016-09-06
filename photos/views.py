bdb3b0d215b7899f4be057fffea59eb850a904a1
Luis Paico <luis@kasfactory.net>
2016-09-06 23:27:14 +0200

    Mejoramos el listado de fotos del inicio recuperando s´ólo las ´últimas fotos

--- photos/views.py
+++ photos/views.py
@@ -9,6 +9,6 @@ def home (request):
     :return: objeto HttpResponse con los datos de la respuesta
     """
 
-    photos = Photo.objects.all() # recupera todas las fotos de la base de datos
-    context = {'photos_list': photos}
+    photos = Photo.objects.all().order_by('-created_at') # recupera todas las fotos de la base de datos
+    context = {'photos_list': photos[:4]}
     return  render(request, 'photos/home.html', context)
